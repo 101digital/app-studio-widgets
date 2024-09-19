64c850d1bfd2f92e541ac0adf10e76d08a4d57e4
Thanh Nguyen <thanhnguyen@Thanhs-Mac-mini.local>
2024-09-19 09:49:53 +0700

    test: remove drop down

--- src/components/tabView/index.tsx
+++ src/components/tabView/index.tsx
@@ -1,61 +1,70 @@
-import React, { ReactNode, useContext } from 'react';
-import { ScrollView, ScrollViewProps, StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import DeviceInfo from 'react-native-device-info';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { ReactNode, useContext } from "react";
+import {
+  ScrollView,
+  ScrollViewProps,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import DeviceInfo from "react-native-device-info";
 import { ThemeContext } from "../../context/theme-context";
 
 export type ASTabViewProps = {
-    children: ReactNode;
-    style?: StyleProp<ViewStyle>;
-    isScrollable?: boolean;
-    scrollViewContentContainerStyle?: StyleProp<ViewStyle>;
-    scrollViewProps?: ScrollViewProps;
-    title?: string;
-    name?: string;
+  children: ReactNode;
+  style?: StyleProp<ViewStyle>;
+  isScrollable?: boolean;
+  scrollViewContentContainerStyle?: StyleProp<ViewStyle>;
+  scrollViewProps?: ScrollViewProps;
+  title?: string;
+  name?: string;
 };
 
 const ASTabView: React.FC<ASTabViewProps> = (props: ASTabViewProps) => {
-    const { colors } = useContext(ThemeContext);
-    const {
-        children,
-        style,
-        isScrollable = true,
-        scrollViewContentContainerStyle,
-        scrollViewProps,
-        title,
-        name,
-        ...restProps
-    } = props;
+  const { colors } = useContext(ThemeContext);
+  const {
+    children,
+    style,
+    isScrollable = true,
+    scrollViewContentContainerStyle,
+    scrollViewProps,
+    title,
+    name,
+    ...restProps
+  } = props;
 
-    return (
-        <View
-            {...restProps}
-            style={[styles.container, { backgroundColor: colors.background }, style]}
+  return (
+    <View
+      {...restProps}
+      style={[styles.container, { backgroundColor: colors.background }, style]}
+    >
+      {isScrollable ? (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          {...scrollViewProps}
+          contentContainerStyle={[
+            styles.scrollViewStyle,
+            scrollViewContentContainerStyle,
+          ]}
         >
-            {isScrollable ? (
-                <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                    {...scrollViewProps}
-                    contentContainerStyle={[styles.scrollViewStyle, scrollViewContentContainerStyle]}
-                >
-                    {children}
-                </ScrollView>
-            ) : (
-                children
-            )}
-        </View>
-    );
+          {children}
+        </ScrollView>
+      ) : (
+        children
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    scrollViewStyle: {
-        flexGrow: 1,
-    },
+  container: {
+    flex: 1,
+  },
+  scrollViewStyle: {
+    flexGrow: 1,
+  },
 });
 
 export default ASTabView;
